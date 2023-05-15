842450232aa32833b767e87147261ffbb0b5e918
Paulina <paulinag2015@wp.pl>
2023-05-16 01:05:58 +0200

    Strona w Nuxt z zakladkami i przyciskiem wywolujacym alert

---
+++ components/navi/TopNav.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="nav">
+        <NuxtLink to='/' class="nav-item">Strona główna</NuxtLink>
+      <NuxtLink to='/about' class="nav-item">O nas</NuxtLink>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'TopNav'
+  }
+  </script>
+  
+  <style scoped>
+  .nav{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 30px;
+    background-color: #2891D7;
+    min-height: 50px;
+    padding-left: 40px;
+    box-shadow: 0px 5px 12px 0px #627988;
+    border-bottom: 5px solid #86BDF1; 
+    margin-bottom: 15px;
+  }
+
+  .nav-item{
+    padding: 16px 5px 10px 5px;
+    color: #d9ebfb;
+    font-family: 'Raleway', sans-serif;
+    font-weight: bold;
+    text-decoration: none;
+    cursor: pointer;
+  }
+
+  .nav-item:hover {
+    background-color: #86BDF1; 
+    color: white;
+  }
+
+  </style>
+  
---
+++ pages/about.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+     <TopNav/>
+      <div class="about">Strona o nas
+
+      <button @click="showInfo">Press me</button>
+    </div>
+  </div>
+  </template>
+  
+  <script>
+  import TopNav from '~/components/navi/TopNav.vue';
+
+  export default {
+    name: 'AboutPage',
+    components: { TopNav },
+
+    methods: {
+        showInfo: () => {
+            alert('Alert');
+            console.log('Konsola')
+        }
+    }
+  }
+  </script>
+  
+  <style scoped>
+
+  .about{
+    padding: 15px 5px 10px 30px;
+    font-family: 'Raleway', sans-serif;
+    font-size:larger;
+    font-size: 20px;
+    gap: 20px;
+  }
+  </style>
+  
